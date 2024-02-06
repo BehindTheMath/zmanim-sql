78b54bb43bbc53781c701328b9dd6174f626b629
Behind The Math <info@behindthemath.info>
2024-02-05 22:53:30 -0500

    Add unit tests for JewishDate

---
+++ tests/hebrewcalendar/jewish-date/run-tests.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+# Load all lines into an array.
+mapfile -t lines < ./tests.psv
+
+echo 'Tests:'
+printf '  %s\n' "${lines[@]}"
+echo '-------'
+
+for line in "${lines[@]}"; do
+#  echo "$line"
+
+  # Split each line into the Gregorian input, and the expected output.
+  # https://stackoverflow.com/a/27521984/8037425
+  read -r gregorian expected <<< "$(echo "$line" | awk -F'|' '{print $1" "$2}')"
+  printf "Running test...\n  Gregorian: '%s'\n  Expected: '%s'\n" "$gregorian" "$expected"
+
+  sql="CALL gregorian_date_to_jewish_date($gregorian, @jewishYear, @jewishMonth, @jewishDay); SELECT @jewishYear, @jewishMonth, @jewishDay;"
+
+  # The `tail` is needed to drop out the following output: `failed to get console mode for stdout: The handle is invalid.`
+  output=$(mysql -u root --batch --database db1 --skip-column-names --execute "$sql" | sed 's/\t/,/g' | tail -n 1)
+  printf "  Output: '%s'\n" "$output"
+
+  if [[ "$output" == "$expected" ]]
+    then
+      echo 'Passed.'
+      echo '-------'
+    else
+      echo "Failed: Expected '$output' to equal '$expected'"
+      exit 1
+  fi
+
+done
